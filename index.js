9593531b3621623e8a05c920eb365a23a157bb4f
Ayush singh <aayushssingh2003@gmail.com>
2025-07-23 19:11:56 +0530

    add crud operations

--- index.js
+++ index.js
@@ -4,13 +4,37 @@ const app = express();
 
 const Port = 3000;
 
-app.get('/' , (req , res) => {
-    res.status(200).send("This is my home page")
+app.use(express.json());
+
+let alcoholsdata = []
+let id = 1;
+
+app.post('/alcohols', (req , res) => {
+    const {name , price , type}=req.body
+    const newalcohol = {
+        id : id++,
+        name,
+        price,
+        type
+    }
+    alcoholsdata.push(newalcohol);
+    res.status(200).send(newalcohol);
 })
 
-app.get('/about' , (req , res) => {
-    res.status(200).send("This is my about page")
+app.get('/alcohols', (req , res) => {
+    res.status(200).send(alcoholsdata);
+} ) 
+
+app.get('/alcohols/:id', (req, res) => {
+    const targetID = parseInt(req.params.id);
+    const data = alcoholsdata[targetID - 1];
+    if(!data){
+       return res.status(404).send("Alcohol not found");
+    }else{
+        res.status(200).send(data);
+    }
 })
+
 
 
 
